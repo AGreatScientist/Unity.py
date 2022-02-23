85d73b9255f159c91a3fec623a03c96b613f93ad
AGreatScientist <92208550+AGreatScientist@users.noreply.github.com>
2022-02-23 15:32:47 +0300

    Create object_3d.py

---
+++ object_3d.py
@@ -0,0 +1,65 @@
+import pygame as pg
+from matrix_functions import *
+from numba import njit
+
+@njit(fastmath=True)
+def any_func(arr, a, b):
+    return np.any((arr == a) | (arr == b))
+
+class Object3D:
+    def __init__(self, render, vertexes='', faces=''):
+        self.render = render
+        self.vertexes = np.array([np.array(v) for v in vertexes])
+        self.faces = np.array([np.array(face) for face in faces])
+        self.translate([0.0001, 0.0001, 0.0001])
+
+        self.font = pg.font.SysFont('Arial', 30, bold=True)
+        self.color_faces = [(pg.Color('orange'), face) for face in self.faces]
+        self.draw_vertexes = False
+        self.label = ''
+
+    def draw(self):
+        self.screen_projection()
+        self.movement()
+        return Object3D(self.render, self.vertexes, self.faces)
+
+    def movement(self):
+        self.rotate_y(-(pg.time.get_ticks() % 0.005))
+
+    def screen_projection(self):
+        vertexes = self.vertexes @ self.render.camera.camera_matrix()
+        vertexes = vertexes @ self.render.projection.projection_matrix
+        vertexes /= vertexes[:, -1].reshape(-1, 1)
+        vertexes[(vertexes > 1) | (vertexes < -1)] = 0
+        vertexes = vertexes @ self.render.projection.to_screen_matrix
+        vertexes = vertexes[:, :2]
+
+        for index, color_face in enumerate(self.color_faces):
+            color, face = color_face
+            polygon = vertexes[face]
+
+            if not any_func(polygon, self.render.H_WIDTH, self.render.H_HEIGHT):
+                pg.draw.polygon(self.render.screen, color, polygon, 1)
+                if self.label:
+                    text = self.font.render(self.label[index], True, pg.Color('white'))
+                    self.render.screen.blit(text, polygon[-1])
+
+        if self.draw_vertexes:
+            for vertex in vertexes:
+                if not any_func(vertex, self.render.H_WIDTH, self.render.H_HEIGHT):
+                    pg.draw.circle(self.render.screen, pg.Color('white'), vertex, 2)
+
+    def translate(self, pos):
+        self.vertexes = self.vertexes @ translate(pos)
+
+    def scale(self, scale_to):
+        self.vertexes = self.vertexes @ scale(scale_to)
+
+    def rotate_x(self, angle):
+        self.vertexes = self.vertexes @ rotate_x(angle)
+
+    def rotate_y(self, angle):
+        self.vertexes = self.vertexes @ rotate_y(angle)
+
+    def rotate_z(self, angle):
+        self.vertexes = self.vertexes @ rotate_z(angle)
